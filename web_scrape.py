af0a533f339af2cbc577a09d838099d9f4faf013
Arvind Shivu Govinday <asgovind@andrew.cmu.edu>
2021-06-07 04:25:18 -0400

    added shard west view

--- web_scrape.py
+++ web_scrape.py
@@ -4,12 +4,18 @@
 from bs4 import BeautifulSoup 
 import requests 
 
-image_url1 = 'https://images-webcams.windy.com/64/1512053164/current/full/1512053164.jpg'
-image_url2 = 'https://images-webcams.windy.com/69/1512053169/current/full/1512053169.jpg'
+image_url1 = 'https://images-webcams.windy.com/64/1512053164/current/full/1512053164.jpg' #london eye
+image_url2 = 'https://images-webcams.windy.com/69/1512053169/current/full/1512053169.jpg' #westminster hall
+image_url3 = 'https://api.deckchair.com/v1/viewer/image/60bdd58beea25f000183056b' #shard west
+
 img_data = requests.get(image_url1).content
 with open('london_eye.jpg', 'wb') as handler:
     handler.write(img_data)
 
 img_data = requests.get(image_url2).content
 with open('westminister.jpg', 'wb') as handler:
+    handler.write(img_data)
+
+img_data = requests.get(image_url3).content
+with open('shard_west.jpg', 'wb') as handler:
     handler.write(img_data)
