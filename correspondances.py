d3c5947565badf2fa0ac41bca6b39d4130622b19
Arvind Shivu Govinday <asgovind@andrew.cmu.edu>
2021-06-30 00:55:18 -0400

    got vanishing points plotted

--- correspondances.py
+++ correspondances.py
@@ -2,17 +2,23 @@
 # Use to get correspondances between two images
 
 import cv2 as cv
+import numpy as np
+
+points = []
 
 def click_event(event, x, y, flags, params):
     if event == cv.EVENT_LBUTTONDOWN:
         print(x, ' ', y)
-        cv.circle(img, (x,y), radius=10, color=(0, 0, 255), thickness=-1)
+        points.append((x,y))
+        cv.circle(img, (x,y), radius=10, color=(0, 0, 255), thickness= -1)
         cv.imshow('image', img)
 
 if __name__ == "__main__":
-    img = cv.imread('london_eye.jpg')
+    img = cv.imread('westminister.jpg')
     cv.imshow('image', img)
     cv.setMouseCallback('image', click_event)
     cv.waitKey(0)
     cv.destroyAllWindows()
+    print(np.shape(points))
+    np.save('westminister_horizontal', points)
 
